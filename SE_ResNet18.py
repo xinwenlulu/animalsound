913ed5eddaaf6d67c72049863d528fb506f2831e
Xin Wen <xin.wen20@imperial.ac.uk>
2021-09-01 22:29:28 +0100

    se-resnet18

--- SE_ResNet18.py
+++ SE_ResNet18.py
@@ -10,22 +10,25 @@ learning_rate = 0.01
 epochs = 100
 patience = 50
 num_trials = 3
-modelname = 'SEResNet18'
+modelname = ['ResNet18', 'SEResNet18']
 augmentation = True
 timemask = 24
-se = True
+se = [False, True]
 
 path_list_dict, partition_size_dict = get_dataset_info(TFRECORDS_FOLDER)
 print(partition_size_dict)
-for i in range(num_trials):
-    name = modelname+str(i)
-    model, history = createSEResNet18(learning_rate, f1_m, precision_m, recall_m, path_list_dict,
-                                      partition_size_dict, epochs=epochs, patience=patience,
-                                      augmentation=augmentation, timemask=timemask, se=se)
-    score, test_y, bool_predict = evaluate_on_test(model, path_list_dict)
-    classification_result(name, test_y, bool_predict)
-    val_f1 = plot_training_metrics(name, history)
-    np.savetxt(figurepath + name + 'valF1.txt', val_f1, fmt='%.2f')
-    plot_confusion_matrix(name, test_y, bool_predict)
+index = 0
+for n, s in zip(modelname, se):
+    name = n
+    for i in range(num_trials):
+        name = n+str(i)
+        model, history = createSEResNet18(learning_rate, f1_m, precision_m, recall_m, path_list_dict,
+                                          partition_size_dict, epochs=epochs, patience=patience,
+                                          augmentation=augmentation, timemask=timemask, se=s)
+        score, test_y, bool_predict = evaluate_on_test(model, path_list_dict)
+        classification_result(name, test_y, bool_predict)
+        val_f1 = plot_training_metrics(name, history)
+        np.savetxt(figurepath + name + 'valF1.txt', val_f1, fmt='%.2f')
+        plot_confusion_matrix(name, test_y, bool_predict)
 
 plot_model(model, to_file=figurepath+modelname+'.png')
